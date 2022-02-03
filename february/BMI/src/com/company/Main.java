4c2aa53db16d465cfa8d2822c690975d918877e3
Mikkel <mikkel.simons1@gmail.com>
2022-02-03 09:40:52 +0100

    Added diagnosis feature to BMI project

--- february/BMI/src/com/company/Main.java
+++ february/BMI/src/com/company/Main.java
@@ -12,16 +12,32 @@ public class Main {
 
         //ASK AND READ USER HEIGHT
         System.out.print("Skriv din højde i cm: ");
-        float height = Integer.parseInt(heightObj.nextLine());
+        float height = Float.parseFloat(heightObj.nextLine());
 
         //ASK AND READ USER WEIGHT
         System.out.print("Skriv din vægt i kilo: ");
-        float weight = Integer.parseInt(weightObj.nextLine());
+        float weight = Float.parseFloat(weightObj.nextLine());
 
         //CREATE VARIABLE BMI
         float BMI = weight / (height * height)*10000;
 
         //PRINT BMI
         System.out.println("Din BMI er: "+BMI);
+
+        //PRINT RESULT
+        if (BMI<18.5){
+            System.out.println("Du er undervægtig");
+        }
+        else if (BMI>=18.5 && BMI < 25){
+            System.out.println("Du har en normal vægt");
+        }
+
+        else if (BMI > 25 && BMI < 30){
+            System.out.println("Du er overvægtig");
+        }
+
+        else {
+            System.out.println("Du er svært overvægtig");
+        }
     }
 }
