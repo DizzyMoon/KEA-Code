ecb18a9bd1621f1908f6c8b241597c61c97ea4ec
Mikkel <mikkel.simons1@gmail.com>
2022-02-03 11:37:40 +0100

    Added invalid input check

--- february/BMI/src/com/company/Main.java
+++ february/BMI/src/com/company/Main.java
@@ -6,38 +6,74 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //CREATE OBJECTS
+        //CREATE VARIABLES
+        boolean invalidData = true;
+        float height = 0;
         Scanner heightObj = new Scanner(System.in);
         Scanner weightObj = new Scanner(System.in);
+        Scanner boolObj = new Scanner(System.in);
 
-        //ASK AND READ USER HEIGHT
-        System.out.print("Skriv din højde i cm: ");
-        float height = Float.parseFloat(heightObj.nextLine());
+        //CHECK IF INPUT IS CORRECT
+        while(invalidData) {
+
+            //ASK AND READ USER HEIGHT
+            System.out.print("Skriv din højde i meter: ");
+            height = Float.parseFloat(heightObj.nextLine());
+
+            //IF USER WROTE IN CENTIMETERS, RECALCULATE TO METERS
+            if (height > 100){
+                System.out.println("Din højde skal skrives i meter, ikke centimeter");
+
+                boolean jaNejInvalid = true;
+
+                //CHECK IF USER WRITES A VALID INPUT (YES OR NO)
+                    while(jaNejInvalid) {
+
+                        System.out.println("Mente du "+height/100+" meter? (ja/ nej)");
+                        String ja_nej = boolObj.nextLine();
+
+                        if (ja_nej.equals("ja")) {
+                            height = height / 100;
+                            invalidData = false;
+                            jaNejInvalid = false;
+                        }
+                        else if (ja_nej.equals("nej")) {
+                            jaNejInvalid = false;
+                        }
+                        else {
+                            System.out.println("Skriv venligst \"ja\" eller \"nej\" i skrivefeltet:");
+                        }
+                    }
+            }
+            else {
+                invalidData = false;
+            }
+        }
 
         //ASK AND READ USER WEIGHT
-        System.out.print("Skriv din vægt i kilo: ");
+        System.out.print("Skriv din vægt i kilogram: ");
         float weight = Float.parseFloat(weightObj.nextLine());
 
         //CREATE VARIABLE BMI
-        float BMI = weight / (height * height)*10000;
+        float BMI = weight / (height * height);
 
         //PRINT BMI
         System.out.println("Din BMI er: "+BMI);
 
         //PRINT RESULT
         if (BMI<18.5){
-            System.out.println("Du er undervægtig");
+            System.out.println("Du er undervægtig :(");
         }
         else if (BMI>=18.5 && BMI < 25){
-            System.out.println("Du har en normal vægt");
+            System.out.println("Du har en normal vægt :)");
         }
 
         else if (BMI > 25 && BMI < 30){
-            System.out.println("Du er overvægtig");
+            System.out.println("Du er overvægtig :(");
         }
 
         else {
-            System.out.println("Du er svært overvægtig");
+            System.out.println("Du er svært overvægtig :(");
         }
     }
 }
