ee496a7e0708acaf62b048f3d46df6c2b83408fb
Mikkel <mikkel.simons1@gmail.com>
2022-02-03 11:44:08 +0100

    fixed issues with BMI

--- february/BMI/src/com/company/Main.java
+++ february/BMI/src/com/company/Main.java
@@ -1,10 +1,11 @@
 package com.company;
-
 import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
+
+        //PRESENTATION
+        System.out.println("Velkommen til Mikkel\'s BMI Beregner :)");
 
         //CREATE VARIABLES
         boolean invalidData = true;
