9203a90c8f83182099463ba3c5b6e81249124758
Mikkel <mikkel.simons1@gmail.com>
2022-02-04 13:09:59 +0100

    Changed tommer to inches

--- february/tommeBeregner/src/com/company/Main.java
+++ february/tommeBeregner/src/com/company/Main.java
@@ -8,11 +8,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         //PRESENTATION
-        System.out.println("Velkommen til Mikkel\'s tomme-beregner :)");
+        System.out.println("Velkommen til Mikkels tomme-beregner :)");
         System.out.print("Skriv et mål i tommer, som skal omregnes til centimeter: ");
 
         //DEFINE VARIABLE AND ASK FOR USER-INPUT
-        float tommer = sc.nextFloat();
+        float inches = sc.nextFloat();
 
         //PRINT RESULT
         System.out.print("Mål i centimeter: "+ tommer * 2.54f);
