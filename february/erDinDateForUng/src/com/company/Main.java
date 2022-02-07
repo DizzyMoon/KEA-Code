d6249fef911a8d3c837a448f37f29bd845e56876
Mikkel <mikkel.simons1@gmail.com>
2022-02-07 12:53:12 +0100

    Fixed formatting in erDinDateForUng

--- february/erDinDateForUng/src/com/company/Main.java
+++ february/erDinDateForUng/src/com/company/Main.java
@@ -16,45 +16,31 @@ public class Main {
     //GET USER INPUTS
     System.out.print("Skriv din alder her: ");
     int userAge = sc.nextInt();
-
-    while(writeNew){ //REVERT POINT (IF USER WRITES JA WHEN PROMPTED)
-
+    while (writeNew) { //REVERT POINT (IF USER WRITES JA WHEN PROMPTED)
       yesNoInvalid = true; //RESET yesNoInvalid
-
       System.out.print("Skriv din date's alder her: ");
       int dateAge = sc.nextInt();
 
       //CHECK IF DATE IS YOUNGER THAN USER
       if (dateAge < userAge) {
-
         int minimumAge = userAge / 2 + 7; //DEFINE MINIMUM AGE
 
-      //CHECK IF DATE IS YOUNGER THAN MINIMUM AGE. PRINT RESULT
-
-        while(yesNoInvalid){ //REVERT POINT (IF USER INPUT IS INVALID)
-
+        //CHECK IF DATE IS YOUNGER THAN MINIMUM AGE. PRINT RESULT
+        while (yesNoInvalid) { //REVERT POINT (IF USER INPUT IS INVALID)
           if (dateAge < minimumAge) {
-             System.out.println("Din date er for ung :(");
-             System.out.print("Skriv din date's alder igen? (ja/nej): ");
-
-             String yesNo = sc.next();
-
-             //CHECK IF USER WRITES YES OR NO
-             if (yesNo.equals("nej")){
-               writeNew = false;
-               yesNoInvalid = false;
-             }
-
-             else if (yesNo.equals("ja")) {  //JUMP TO LINE 22
-               yesNoInvalid = false;
-             }
-
-             else {
-               System.out.println("Skriv venligst \"ja\" eller \"nej\" i skrivefeltet");
-             }
-
-          }
-          else {
+            System.out.println("Din date er for ung :(");
+            System.out.print("Skriv din date's alder igen? (ja/nej): ");
+            String yesNo = sc.next();
+            //CHECK IF USER WRITES YES OR NO
+            if (yesNo.equals("nej")) {
+              writeNew = false;
+              yesNoInvalid = false;
+            } else if (yesNo.equals("ja")) {  //JUMP TO LINE 22
+              yesNoInvalid = false;
+            } else {
+              System.out.println("Skriv venligst \"ja\" eller \"nej\" i skrivefeltet");
+            }
+          } else {
             yesNoInvalid = false;
             writeNew = false;
             System.out.println("Din date er ikke for ung ;)");
