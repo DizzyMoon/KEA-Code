5f83c60e2b646e17677c6e1f48ea27d70ebe3e56
DizzyMoon <mikkel.simons1@gmail.com>
2022-02-17 12:08:20 +0100

    Edited lineBattle

--- february/lineBattle/src/com/company/lineBattle.java
+++ february/lineBattle/src/com/company/lineBattle.java
@@ -55,16 +55,18 @@ public class lineBattle {
 
   public void scoutStatus() {
 
-    findHighestNumber(playerLocation, enemyLocation);
-
-    if (highestNumber - lowestNumber == 2) {
+    if (findDistance() == 2) {
       System.out.println("SCOUT MESSAGE: ENEMY FORCES ARE ONLY TWO SPACES AWAY!");
-    } else if (highestNumber - lowestNumber == 1) {
+    } else if (findDistance() == 1) {
       System.out.println("SCOUT MESSAGE: ENEMY FORCES ARE ONLY ONE SPACE AWAY!");
     }
   }
 
   public void upkeep() {
+    if (playerAmmunition <= 0 || playerLocation == -10 || playerSoldierCount <= 0){
+      gameOver = true;
+    }
+
     if (playerTurn) {
       playerTurn = false;
     } else {
@@ -91,32 +93,73 @@ public class lineBattle {
     if (action.equals("f") || action.equals("F")) {
       System.out.println("FULL MARCH!");
       actionForward();
-    } else if (action.equals("r") || action.equals("R")){
+    } else if (action.equals("r") || action.equals("R")) {
       System.out.println("TACTICAL RETREAT!");
       actionRetreat();
     }
   }
 
-  public void enemyTurn(){
+  public void enemyTurn() {
+    int roll = throwDice();
+    if (roll < 3){
+      actionForward();
+      System.out.println("ENEMY MOVES FORWARD!");
+    } else if (roll < 5){
+      actionAttack();
+      System.out.println("ENEMY ATTACKS!");
+    } else {
+      actionRetreat();
+      System.out.println("ENEMY RETREATS!");
+    }
 
   }
 
-  public void actionAttack(){
-    
+  public int findDistance() {
+    findHighestNumber(playerLocation, enemyLocation);
+    int distance = highestNumber - lowestNumber;
+    return distance;
   }
 
-  public void actionRetreat(){
+  public void actionAttack() {
+    findHighestNumber(playerLocation, enemyLocation);
+    int ammoCost = throwDice() * 100;
+
+    if (playerTurn) {
+      playerAmmunition = playerAmmunition - ammoCost;
+
+      switch (findDistance()) {
+        case 0 -> enemySoldierCount = enemySoldierCount - 6;
+        case 1 -> enemySoldierCount = enemySoldierCount - 5;
+        case 2 -> enemySoldierCount = enemySoldierCount - 4;
+        case 3 -> enemySoldierCount = enemySoldierCount - 3;
+        case 4 -> enemySoldierCount = enemySoldierCount - 2;
+        case 5 -> enemySoldierCount = enemySoldierCount - 1;
+      }
+    } else {
+      switch (findDistance()) {
+        case 0 -> playerSoldierCount = playerSoldierCount - 6;
+        case 1 -> playerSoldierCount = playerSoldierCount - 5;
+        case 2 -> playerSoldierCount = playerSoldierCount - 4;
+        case 3 -> playerSoldierCount = playerSoldierCount - 3;
+        case 4 -> playerSoldierCount = playerSoldierCount - 2;
+        case 5 -> playerSoldierCount = playerSoldierCount - 1;
+      }
+    }
+  }
+
+
+  public void actionRetreat() {
     int roll = throwDice();
     int move;
     if (roll < 3) {
       move = 1;
-    } else if (roll < 5){
+    } else if (roll < 5) {
       move = 2;
     } else {
       move = 3;
     }
 
-    if (playerTurn){
+    if (playerTurn) {
       System.out.println("YOU ROLLED : " + roll);
       playerLocation = playerLocation + move;
       playerAmmunition = playerAmmunition + 250;
@@ -125,11 +168,10 @@ public class lineBattle {
       enemyLocation = enemyLocation - move;
       enemyAmmunition = enemyAmmunition + 250;
     }
+    upkeep();
   }
 
   public void execute() {
-
-
     System.out.println("WELCOME TO LINE BATTLE SOLDIER");
 
     while (!gameOver) {
@@ -139,7 +181,9 @@ public class lineBattle {
         enemyTurn();
       }
     }
-  }
+    System.out.println("GAME OVER!");
+    }
+
 
   public static void main(String[] args) {
     new lineBattle().execute();
