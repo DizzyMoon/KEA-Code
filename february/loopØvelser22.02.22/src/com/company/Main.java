a1296b2f38aba6268be33d1f6604c205034868a8
DizzyMoon <mikkel.simons1@gmail.com>
2022-02-22 11:52:34 +0100

    Added multiplication table in loopØvelser for 22.02.22

--- february/loopØvelser22.02.22/src/com/company/Main.java
+++ february/loopØvelser22.02.22/src/com/company/Main.java
@@ -19,6 +19,36 @@ public class Main {
     System.out.println(input.charAt(i));
   }
 
+  public void starBlock(int width, int height) {
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+
+  public void multiplicationTable() {
+    int i;
+    int j;
+
+    for (i = 1; i <= 10; i++) {
+      for (j = 1; j <= 10; j++) {
+        System.out.printf("%-5d", i*j);
+      }
+      System.out.println();
+    }
+  }
+
+  public void yxLoop() {
+    for (int x = 0; x < 3; x++) {
+      for (int y = 0; y < 3; y++) {
+        System.out.printf("x: %d, y:%d,", x, y);
+      }
+      System.out.println();
+    }
+  }
+
   public void findIndexOf() {
     System.out.println("Write search-character");
 
@@ -54,6 +84,7 @@ public class Main {
       uniC = input.codePointAt(i);
       String toUni = Character.toString((char) uniC);
       String toUniUpper = Character.toString((char) uniC - 32);
+
       if ((uniC >= 97 && uniC <= 122) || uniC >= 224 && uniC <= 255) {
         if (!(input.charAt(i) == ' ')) {
           sb.append(toUniUpper);
@@ -91,6 +122,11 @@ public class Main {
     reverseString();
     System.out.println("Print indexes");
     findIndexOf();
+    System.out.println("yxLoop");
+    yxLoop();
+    starBlock(5, 7);
+    System.out.println("Multiplication Table");
+    multiplicationTable();
   }
 
   //MAIN
