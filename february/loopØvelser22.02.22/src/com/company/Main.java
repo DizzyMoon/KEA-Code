130e7b7b7d3847ee2fbc66ce0a9a177781578df9
DizzyMoon <mikkel.simons1@gmail.com>
2022-02-22 10:40:33 +0100

    Finished loopØvelser for 22.02.22

--- february/loopØvelser22.02.22/src/com/company/Main.java
+++ february/loopØvelser22.02.22/src/com/company/Main.java
@@ -21,13 +21,11 @@ public class Main {
 
   public void findIndexOf() {
     System.out.println("Write search-character");
-    input = sc.nextLine();
     char charAt = input.charAt(0);
     boolean found = false;
 
     String str = "This is a very cool string";
     for (int i = 0; i < str.length() - 1; i++) {
-
       if (str.charAt(i) == charAt) {
         System.out.println(i);
         found = true;
@@ -39,15 +37,34 @@ public class Main {
   }
 
   public void reverseString() {
-
     sb.setLength(0);
     int i;
+    
     for (i = input.length(); i > 0; i--) {
       sb.append(input.charAt(i - 1));
     }
     System.out.println(sb);
+  }
 
+  public void toUpperCase() {
+    sb.setLength(0);
+    int uniC;
 
+    for (int i = 0; i < input.length(); i++) {
+      uniC = input.codePointAt(i);
+      String toUni = Character.toString((char) uniC);
+      String toUniUpper = Character.toString((char) uniC - 32);
+      if ((uniC >= 97 && uniC <= 122) || uniC >= 224 && uniC <= 255) {
+        if (!(input.charAt(i) == ' ')) {
+          sb.append(toUniUpper);
+        } else {
+          sb.append(' ');
+        }
+      } else {
+        sb.append(toUni);
+      }
+    }
+    System.out.println(sb);
   }
 
   public void mOcKiFy() {
@@ -68,11 +85,12 @@ public class Main {
     printOneCharAtATime();
     System.out.println("Print Mockification");
     mOcKiFy();
+    System.out.println("To upper case function");
+    toUpperCase();
     System.out.println("Print reverse string");
     reverseString();
     System.out.println("Print indexes");
     findIndexOf();
-
   }
 
   //MAIN
