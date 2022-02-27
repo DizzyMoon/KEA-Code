016fb45c92a13c6d5acf8790aac8e13864aa2d78
DizzyMoon <mikkel.simons1@gmail.com>
2022-02-28 00:40:22 +0100

    Added arrayIndexOf

---
+++ february/uge_8/arrayIndexOf/src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public int arrayIndexOf(String[] arrayIn, String input){
+        for(int i = 0; i < arrayIn.length; i++){
+            if (arrayIn[i].equals(input)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int arrayIndexOf(int[] arrayIn, int input){
+        for(int i = 0; i < arrayIn.length; i++){
+            if (arrayIn[i] == input){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int arrayIndexOf(double[] arrayIn, double input){
+        for(int i = 0; i < arrayIn.length; i++){
+            if (arrayIn[i] == input){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void execute(){
+
+    }
+
+    public static void main(String[] args) {
+        new Main().execute();
+    }
+}
